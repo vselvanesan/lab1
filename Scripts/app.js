003ab29f8b9a0b456e332b57fd2a8ea06697600c
Nesan <vselvane@my.centennialcollege.a>
2018-05-09 06:50:53 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,11 @@
+/* Custom JS goes here */
+// IIFE - Immediately Invoked Function Expression
+// anonymus self executing function
+
+(function(){
+    function Start(){
+       console.log ("App Strted!");
+    }
+    window.onload = Start;
+})();
+
